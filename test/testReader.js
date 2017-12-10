927a7c9702c1a04480f2b60fdb18c5565bfa1b06
Wallace Gibbon <wallacegibbon@aliyun.com>
2017-12-10 19:57:10 +0800

    escape sequence problem in string and character solved

--- test/testReader.js
+++ test/testReader.js
@@ -1,20 +1,20 @@
 const { EndOfFile } = require("../lib/errors");
 const Reader = require("../lib/Reader");
 
-const text = `int main(int argc, char **argv)
+const text = `int test(int argc, char **argv, ...)
 {
-  float f = 3.1415926;
+  float f1 = 3.1415926, f2 = .15;
   int i = 123;
-  char c = 'c';
-  //$errorsym
-  /*
-  //char *s = "abcdefg\\hijklmn";
-  printf("\"f\" is %f, and \"i\" is %d\n", f, i);
-  */
+  char c1 = 'c', c2 = '\x65';
+  char *s = "abcdefg\\\\hijklmn";
+  printf("\\"f\\" is %f, and \\"i\\" is %d\\n", f, i);
   //char *s = "abcdefghijklmn";
-  $errorsym
+  /*
   char *s = "abcdefghijklmn
              invalid newline";
+  */
+  //$errorsym
+  char *s = "test escape: \\x65, \\a, \\n";
   return 0;
 }
 `;
@@ -25,8 +25,9 @@ const reader = new Reader(Buffer.from(text));
 function testGetChar() {
   try {
     while (true) {
+      const stat = reader.currentState();
+      console.log("line:", stat.line, ", ch:", String.fromCharCode(stat.ch));
       reader.getChar();
-      console.log(reader.currentState());
     }
   } catch (e) {
     if (e.constructor !== EndOfFile)
