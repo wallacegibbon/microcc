c423c3070770c499eca7f77a322eb281a5a7743f
Wallace Gibbon <wallacegibbon@aliyun.com>
2017-12-11 21:45:17 +0800

    fixed bug in getCharacter, and added token for double plus and double minus

--- test/testGetToken.js
+++ test/testGetToken.js
@@ -1,32 +1,13 @@
 const { EndOfFile } = require("../lib/errors");
 const Reader = require("../lib/Reader");
+const fs = require("fs");
 
-const text = `int test(int argc, char **argv, ...)
-{
-  float f1 = 3.1415926, f2 = .15;
-  int i = 123;
-  char c1 = 'c', c2 = '\x65';
-  char *s = "abcdefg\\\\hijklmn";
-  printf("\\"f\\" is %f, and \\"i\\" is %d\\n", f, i);
-  //char *s = "abcdefghijklmn";
-  /*
-  char *s = "abcdefghijklmn
-             invalid newline";
-  */
-  //$errorsym
-  char *s = "test escape: \\x65, \\a, \\n";
-  i >= 3 && i <= 5 || i%2==1;
-  return 0;
-}
-`;
-
+const text = fs.readFileSync(`${__dirname}/testToken.c`);
 const reader = new Reader(Buffer.from(text));
 
-
 try {
-  while (true) {
+  while (true)
     console.log(reader.getToken());
-  }
 } catch (e) {
   if (e.constructor !== EndOfFile)
     console.log(`line ${e.line}: ${e.message}`);
--- test/testTokenize.js
+++ test/testTokenize.js
@@ -1,30 +1,12 @@
 const { EndOfFile } = require("../lib/errors");
 const Reader = require("../lib/Reader");
+const fs = require("fs");
 
-const text = `int test(int argc, char **argv, ...)
-{
-  float f1 = 3.1415926, f2 = .15;
-  int i = 123;
-  char c1 = 'c', c2 = '\x65';
-  char *s = "abcdefg\\\\hijklmn";
-  printf("\\"f\\" is %f, and \\"i\\" is %d\\n", f, i);
-  //char *s = "abcdefghijklmn";
-  /*
-  char *s = "abcdefghijklmn
-             invalid newline";
-  */
-  //$errorsym
-  char *s = "test escape: \\x65, \\a, \\n";
-  i >= 3 && i <= 5 || i%2==1;
-  return 0;
-}
-`;
-
+const text = fs.readFileSync(`${__dirname}/testToken.c`);
 
 try {
   console.log(new Reader(Buffer.from(text)).tokenize());
 } catch (e) {
-  if (e.constructor !== EndOfFile)
-    console.log(`line ${e.line}: ${e.message}`);
+  console.log(`line ${e.line}: ${e.message}`);
 }
 
