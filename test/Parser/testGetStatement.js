ff4c464bdb3bfa883263ad178c24bb10aefeacd0
wallacegibbon <wallacegibbon@aliyun.com>
2017-12-12 12:42:30 +0800

    ready to write parser

--- test/Parser/testGetStatement.js
+++ test/Parser/testGetStatement.js
@@ -1,13 +1,13 @@
 const { EndOfFile } = require("../../lib/errors");
-const Reader = require("../../lib/Reader");
+const Parser = require("../../lib/Parser");
 const fs = require("fs");
 
 const text = fs.readFileSync(`${__dirname}/a.c`);
-const reader = new Reader(Buffer.from(text));
+const parser = new Parser(Buffer.from(text));
 
 try {
   while (true)
-    console.log(reader.getToken());
+    console.log(parser.getStatement());
 } catch (e) {
   if (e.constructor !== EndOfFile)
     console.log(`line ${e.line}: ${e.message}`);
